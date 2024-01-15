f0f256f2742104b24a7bf99450a27f28ad0a1d3a
GloriaMortis <2x2priboy@gmail.com>
2024-01-16 00:38:04 +0300

    correct line

--- src/item.py
+++ src/item.py
@@ -3,18 +3,22 @@ class Item:
     pay_rate = 1.0
     all = []
 
+
     def __init__(self, name, price, quantity):
         self.name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
 
+
     def calculate_total_price(self):
         total_price = self.price * self.quantity
 
         return total_price
 
+
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
         return self.price
+

322694bd03c1deb6ff9627297f88419e9cdb25aa
GloriaMortis <2x2priboy@gmail.com>
2024-01-15 23:57:25 +0300

    add comments for test and change testfile

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,33 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+class Test_Item:
+
+    pay_rate = 1.0
+    all = []
+
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        Test_Item.all.append(self)
+
+    def test_calculate_total_price(self) -> float:
+        total_price = self.price * self.quantity
+
+        return total_price
+
+    def test_apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+        return self.price
+
+
+if __name__ == '__main__':
+    item1 = Test_Item("apple", 100000, 4)
+
+    #test 1
+    assert item1.test_calculate_total_price()
+
+    #test 2
+    assert item1.test_apply_discount()
+
+#К сожалению у меня пока не решен вопрос по установке Pytest, по этому тесты сделал через assert
